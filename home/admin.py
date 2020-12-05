dcb6f2e9201ea2711fb74bed21c8f1ef19b8711a
tariq-sf <tariq-yousef-@hotmail.com>
2020-12-05 18:34:35 +0800

    add jobd views&url

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import home , Category
+
+admin.site.register(home)
+
+admin.site.register(Category)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+'''
+django models field :
+ -html widget
+ -validation
+ -db size
+ '''
+JOB_TYPE = (
+    ('Full time','Full time'),
+    ('part time','part time'),
+)
+class home(models.Model):  #table
+    title = models.CharField(max_length=100) #column
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE) #column
+    Description = models.TextField(max_length=1000) #column
+    salary=models.IntegerField(default=0)
+    published_at =models.DateTimeField(auto_now=True)
+    experience=models.IntegerField(default=2)
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+class Category (models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name 
+
+ 
