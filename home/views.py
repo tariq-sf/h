b7001071f760f89569a9dbeace2d66ddbd51c9a8
tariq-sf <tariq-yousef-@hotmail.com>
2020-12-09 23:57:22 +0800

    'data'

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import home
+
+
 # Create your views here.
+
+def home_list(request):
+    home_list=home.objects.all()
+    
+    context={'homes':home_list}
+    return render(request,'home/home_list.html',context)
+
+def home_detail(request):
+    return HttpResponse('delait view')
